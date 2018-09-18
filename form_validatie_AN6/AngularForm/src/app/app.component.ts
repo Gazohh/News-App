62abf60864eac4ae77f6c41710c38cf6c5d94bbd
Ramon J <ramonaventus@gmail.com>
2018-09-18 13:35:30 +0200

    V2.7
    Angular 6 - Form Validatie

---
+++ form_validatie_AN6/AngularForm/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import {passValidator} from './custom-validators'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  rForm: FormGroup;
+  post: any;
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  cnfpassword: string = '';
+
+  constructor(fb: FormBuilder){
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'email': [null, [Validators.required, Validators.email]],
+      'password': [null, Validators.required],
+      'cnfpassword': [null, passValidator]
+
+    });
+  }
+
+  addForm(post){
+
+  }
+
+}
