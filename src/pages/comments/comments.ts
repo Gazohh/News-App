32daaf477586e04aeb2de4e847d33e1290b441bf
Gazohh <39297625+Gazohh@users.noreply.github.com>
2018-11-08 14:31:48 +0100

    Profielfoto gefixt

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 /**
  * Generated class for the CommentsPage page.
@@ -15,11 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CommentsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  dataUser: any;
+  username: string;
+  pictureprofile: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CommentsPage');
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+    const headers = new HttpHeaders();
 
+    headers.append("Accept", 'application/json');
+
+    headers.append('Content-Type', 'application/json');
+
+    const options = { headers: headers };
+
+    const data = {
+
+      email: localStorage.getItem('userEmail'),
+
+    };
+    this.http.post('http://gazoh.net/getgebruiker.php', data, options)
+      .subscribe(data => {
+      this.dataUser = data;
+        this.username = this.dataUser.username;
+        this.pictureprofile = this.dataUser.profilepicture;
+      });
+    }
 }
