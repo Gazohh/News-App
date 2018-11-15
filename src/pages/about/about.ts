68e49a186c3d1949b0b25732047ddd2afef87a8a
Ramon J <ramonaventus@gmail.com>
2018-11-15 13:16:43 +0100

    About page in slides gemaakt met text erin

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FeedPage} from "../feed/feed";
+import {SettingsPage} from "../settings/settings";
 
 /**
  * Generated class for the AboutPage page.
@@ -10,16 +12,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html',
+    selector: 'page-about',
+    templateUrl: 'about.html',
 })
 export class AboutPage {
-  themeStatus:any = localStorage.getItem("themeColor");
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    themeStatus: any = localStorage.getItem("themeColor");
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AboutPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
 
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad AboutPage');
+    }
+
+    slides = [
+        {
+            title: "Welkom bij NewsAge!",
+            description: "Hallo! Welkom op NewsAge! ten eerste willen wij, namens het team, u bedanken voor het gebruiken van NewsAge!",
+            image: "../../assets/imgs/ica-slidebox-img-1.png",
+
+        },
+        {
+            title: "Wat is \"NewsAge\"",
+            description: "NewsAge is de perfecte app om alle nieuws in een oogopslag te lezen.\n" +
+                "        NewsAge maakt het gemakkelijk voor de gebruiker(jij) door alle nieuws te verzamelen van verschillende bronnen en\n" +
+                "        die in een feed te verwerken zodat alles netjes en geordend is ;).",
+            image: "../../assets/imgs/ica-slidebox-img-2.png",
+        },
+        {
+            title: "Waarom NewsAge?",
+            description: "NewsAge is heel gemakkelijk te gebruiken, u kunt bijvoorbeeld met 1 klik een dagblad volgen of niet volgen. U\n" +
+                "kunt ook ervoor kiezen om bijvoorbeeld een artikel te verbergen, dit zorgt ervoor dat u de artikel niet\n" +
+                "        ziet.",
+            image: "../../assets/imgs/ica-slidebox-img-3.png",
+        }
+    ];
+
+    overOnsDone() {
+        this.navCtrl.setRoot(SettingsPage);
+    }
 }
