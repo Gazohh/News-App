f5bb766db925bc8726db926b051537bcc6c5faec
Batuhan Beyogullari <batuhan.beyogullari@student.graafschapcollege.nl>
2018-11-26 12:44:45 +0100

    De array is nu gevuld met favorieten

--- src/pages/favorieten/favorieten.ts
+++ src/pages/favorieten/favorieten.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 /**
  * Generated class for the FavorietenPage page.
@@ -10,11 +11,38 @@ import { NavController, NavParams } from 'ionic-angular';
 
 
 @Component({
-  selector: 'page-favorieten',
-  templateUrl: 'favorieten.html',
+    selector: 'page-favorieten',
+    templateUrl: 'favorieten.html',
 })
 export class FavorietenPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+    likedarticles: any;
+
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public http: HttpClient) {
+
+    }
+
+    ionViewWillEnter()
+    {
+        this.getFavs(localStorage.getItem('userId'));
+    }
+
+    getFavs(userId) {
+        const headers = new HttpHeaders();
+        headers.append("Accept", 'application/json');
+        headers.append('Content-Type', 'application/json');
+        const options = {headers: headers};
+        const data = {
+            user: userId
+        };
+
+        this.http.post('http://www.gazoh.net/getliked.php', data, options).subscribe(res => {
+            this.likedarticles = res;
+            console.log(res);
+        })
+
+    }
 
 }
