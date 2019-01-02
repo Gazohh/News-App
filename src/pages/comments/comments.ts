cdd0fd1001ea22e7cb26de341dc0c18d2dd9c546
Gazohh <39297625+Gazohh@users.noreply.github.com>
2019-01-02 11:50:55 +0100

    comment css fix, feed pagina title en sources fix

--- src/pages/comments/comments.ts
+++ src/pages/comments/comments.ts
@@ -4,6 +4,8 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {AlertController} from 'ionic-angular';
 import {FeedPage} from "../feed/feed";
 import {Content} from 'ionic-angular';
+import { Keyboard } from '@ionic-native/keyboard';
+
 
 @IonicPage()
 @Component({
@@ -31,7 +33,10 @@ export class CommentsPage {
                 public navParams: NavParams,
                 public http: HttpClient,
                 public alertCtrl: AlertController,
-                public events: Events) {
+                public events: Events,
+                public keyboard: Keyboard) {
+
+        keyboard.disableScroll(true);
 
         if (this.navParams.get("record")) {
             this.selectEntry(this.navParams.get("record"));
@@ -61,14 +66,12 @@ export class CommentsPage {
     // -------------------------------------------------
 
     callFunction() {
-        this.content.scrollToBottom(0)
+        this.content.scrollToBottom(200);
     }
 
     // Runs when the page is about to enter and become the active page.
     ionViewDidEnter() {
-        let dimensions = this.content.getContentDimensions();
-        this.content.scrollTo(0, dimensions.contentHeight + 100, 100);
-        setTimeout(() => this.myInput.setFocus(), 250);
+        setTimeout(() => this.myInput.setFocus(), 200);
     }
 
     ionViewDidLoad() {
